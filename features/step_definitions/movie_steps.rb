225f26bb71422ea7611b555b226f3f282e8a2d31
hbakir <hicham@bakir.ch>
2012-11-03 15:47:38 +0100

    Completed Scenario 1 for hw4

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,3 +4,8 @@ Given /^the following movies exist:$/ do |movies_table|
     Movie.create(movie)
   end
 end
+
+Then /^the director of "([^"]*)" should be "([^"]*)"$/ do |title, director|
+  movie = Movie.where("title = ?", title).first
+  movie.director == director
+end
